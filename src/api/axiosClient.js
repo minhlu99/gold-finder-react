b64fd008d8ad66dc4e769e8557280d471a676b00
Minh Lu <lunguyenminh10@gmail.com>
2023-08-09 15:38:46 +0700

    fix code

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3009',
+  baseURL: 'https://api-gold-finder.techwiz.tech',
   headers: {
     'Content-Type': 'application/json'
   },
